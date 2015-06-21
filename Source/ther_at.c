bb90d5cded4b2834ad5ceb411b862f9b791e3d5a
Ganesh Mahendran <opensource.ganesh@gmail.com>
2015-06-21 18:14:12 +0800

    AT: add ther_at.[c | h]
    Signed-off-by: Ganesh Mahendran <opensource.ganesh@gmail.com>

---
+++ Source/ther_at.c
@@ -0,0 +1,39 @@
+
+#include "Comdef.h"
+#include "OSAL.h"
+#include "hal_board.h"
+#include "string.h"
+
+#include "ther_uart.h"
+#include "thermometer.h"
+
+#define MODULE "[THER AT] "
+
+#define AT_CMD "AT"
+
+void ther_at_handle(unsigned char *buf, unsigned char len, unsigned char **ret_buf, unsigned char *ret_len)
+{
+
+	if (memcmp(buf, AT_CMD, strlen(AT_CMD))) {
+		/* not AT CMD */
+		return;
+	}
+
+	if (buf[len - 1] == '\n' && buf[len - 2] == '\r') {
+		len -= 2;
+		buf[len] = '\0';
+	} else if (buf[len - 1] == '\n') {
+		len -= 1;
+		buf[len] = '\0';
+	} else {
+		print(LOG_INFO, "unknown\r\n");
+		return;
+	}
+
+	/* AT CMD */
+	if (len == strlen(AT_CMD)) {
+		return;
+	}
+
+
+}
---
+++ Source/ther_at.h
@@ -0,0 +1,8 @@
+
+#ifndef __THER_AT_H__
+#define __THER_AT_H__
+
+void ther_at_handle(unsigned char *buf, unsigned char len, unsigned char **ret_buf, unsigned char *ret_len);
+
+#endif
+
