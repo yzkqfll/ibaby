426ed55e3240b55e09eac503d6a07e0b89af412e
Ganesh Mahendran <opensource.ganesh@gmail.com>
2015-06-23 10:53:50 +0800

    AT: add AT cmd basic code
    Signed-off-by: Ganesh Mahendran <opensource.ganesh@gmail.com>

--- Source/ther_at.c
+++ Source/ther_at.c
@@ -1,8 +1,11 @@
 
 #include "Comdef.h"
 #include "OSAL.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "hal_board.h"
-#include "string.h"
 
 #include "ther_uart.h"
 #include "thermometer.h"
@@ -10,30 +13,58 @@
 #define MODULE "[THER AT] "
 
 #define AT_CMD "AT"
+#define AT_CAL "AT+CAL="
+#define AT_CAL_QEREY "AT+CAL?"
 
-void ther_at_handle(unsigned char *buf, unsigned short len, unsigned char *ret_buf, unsigned short *ret_len)
+static void at_set_cal_mode(bool mode)
 {
+	print(LOG_INFO, "set cal mode %d\n", mode);
+}
 
-	if (memcmp(buf, AT_CMD, strlen(AT_CMD))) {
-		/* not AT CMD */
+void ther_at_handle(char *cmd_buf, unsigned short len, char *ret_buf, unsigned short *ret_len)
+{
+	char *p;
+
+	*ret_len = 0;
+
+	/* check cmd end */
+	if (cmd_buf[len - 1] != '\n') {
+		print(LOG_INFO, "cmd end with %c, should be \\n \n", cmd_buf[len - 1]);
 		return;
 	}
 
-	if (buf[len - 1] == '\n' && buf[len - 2] == '\r') {
-		len -= 2;
-		buf[len] = '\0';
-	} else if (buf[len - 1] == '\n') {
-		len -= 1;
-		buf[len] = '\0';
+	/* remove \n */
+	cmd_buf[len - 1] = '\0';
+	len--;
+/*	print(LOG_INFO, "get cmd <%s>, len %d\n", cmd_buf, len);*/
+
+	if (strncmp(cmd_buf, AT_CMD, strlen(AT_CMD))) {
+		print(LOG_INFO, "not AT cmd\n");
+		return;
+	}
+
+	if (strcmp(cmd_buf, AT_CMD) == 0) {
+		*ret_len = sprintf((char *)ret_buf, "%s\n", "OK");
+
+	} else if (strncmp((char *)cmd_buf, AT_CAL, strlen(AT_CAL)) == 0) {
+		p = cmd_buf + strlen(AT_CAL);
+		if (*p == '1') {
+			at_set_cal_mode(TRUE);
+		} else if (*p == '0') {
+			at_set_cal_mode(FALSE);
+		} else {
+/*			print(LOG_INFO, "buf <%s>, len %d, unknown cal mode\n",
+					cmd_buf, len);*/
+			*ret_len = sprintf((char *)ret_buf, "%s\n", "ERROR");
+		}
+
+	} else if (strncmp((char *)cmd_buf, AT_CAL_QEREY, strlen(AT_CAL_QEREY)) == 0) {
+
 	} else {
-		print(LOG_INFO, "unknown\r\n");
-		return;
+/*		*ret_len = sprintf((char *)ret_buf, "UNKNOWN CMD\n");*/
+		print(LOG_INFO, "UNKNOWN CMD\n");
 	}
 
-	/* AT CMD */
-	if (len == strlen(AT_CMD)) {
-		return;
-	}
-
+	return;
 
 }
--- Source/ther_at.h
+++ Source/ther_at.h
@@ -2,7 +2,7 @@
 #ifndef __THER_AT_H__
 #define __THER_AT_H__
 
-void ther_at_handle(unsigned char *buf, unsigned short len, unsigned char *ret_buf, unsigned short *ret_len);
+void ther_at_handle(char *cmd_buf, unsigned short len, char *ret_buf, unsigned short *ret_len);
 
 #endif
 
