439d4547f55a8dbd381b5eae46ff802c337e9601
Ganesh Mahendran <opensource.ganesh@gmail.com>
2015-06-25 20:56:59 +0800

    temp cal: seperate ch0/ch1 Rt calculation
    Signed-off-by: Ganesh Mahendran <opensource.ganesh@gmail.com>

--- Source/ther_temp_cal.c
+++ Source/ther_temp_cal.c
@@ -10,30 +10,34 @@
 
 #define MODULE "[TEMP CAL] "
 
-float temp_cal_get_res_by_adc(unsigned char channel, unsigned short adc_val)
+float temp_cal_get_res_by_ch0(unsigned short adc_val)
 {
-	float Rsensor = 0;
+	float Rt = 0;
 
-	if (channel == HAL_ADC_CHANNEL_0) {
-		/*
-		 * From the Schematics
-		 *
-		 * 		(Vsensor * (Rsensor / (Rsensor + R9)) - Vsensor * (R50 / (R49 + R50))) * 5 = Vsensor * (adc / 8192)
-		 * 	=>	Rsensor = R9 / (1 / (adc / (5 * 8192) + R50 / (R49 + R50)) - 1)
-		 */
-		Rsensor = 56.0 / (1.0 / ((float)adc_val / 40960.0 + 56.0 / (56.0 + 76.8)) - 1);
+	/*
+	 * From the Schematics
+	 *
+	 * 		(Vsensor * (Rt / (Rt + R9)) - Vsensor * (R50 / (R49 + R50))) * 5 = Vsensor * (adc / 8192)
+	 * 	=>	Rt = R9 / (1 / (adc / (5 * 8192) + R50 / (R49 + R50)) - 1)
+	 */
+	Rt = 56.0 / (1.0 / ((float)adc_val / 40960.0 + 56.0 / (56.0 + 76.8)) - 1);
 
-	} else if (channel == HAL_ADC_CHANNEL_1) {
-		/*
-		 * From the Schematics
-		 *
-		 * 		R9 / Rsensor = Vr9 / Vsensor       R9 = 56K, Vr9 = (Vref / 8192) * (8192 - adc), Vsensor = (Vref / 8192) * adc
-		 * => 	Rsensor = 56K * adc / (8192 - adc)
-		 */
-		Rsensor = 56.0 * (float)adc_val / (8192.0 - (float)adc_val);
-	}
+	return Rt;
+}
 
-	return Rsensor;
+float temp_cal_get_res_by_ch1(unsigned short adc_val)
+{
+	float Rt = 0;
+
+	/*
+	 * From the Schematics
+	 *
+	 * 		R9 / Rt = Vr9 / Vsensor       R9 = 56K, Vr9 = (Vref / 8192) * (8192 - adc), Vsensor = (Vref / 8192) * adc
+	 * => 	Rt = 56K * adc / (8192 - adc)
+	 */
+	Rt = 56.0 * (float)adc_val / (8192.0 - (float)adc_val);
+
+	return Rt;
 }
 
 /*
@@ -46,10 +50,10 @@ unsigned short temp_cal_get_temp_by_res(float res)
 	/*
 	 * From Sensor SPEC:
 	 *
-	 * 3950.0 = ln(R25/Rsensor) / (1 / (25 + 273.15) - 1 / (temp + 273.15))
+	 * 3950.0 = ln(R25/Rt) / (1 / (25 + 273.15) - 1 / (temp + 273.15))
 	 * 	R25 is the resistance in 25 du
 	 *
-	 * temp = 1 / (1 / (25 + 273.15) - ln(Rt25/Rsensor) / 3950)) - 273.15
+	 * temp = 1 / (1 / (25 + 273.15) - ln(Rt25/Rt) / 3950)) - 273.15
 	 */
 	temp = 1.0 / (1.0 / (25.0 + 273.15) - log(100.0 / res) / 3950) - 273.15;
 
--- Source/ther_temp_cal.h
+++ Source/ther_temp_cal.h
@@ -2,7 +2,8 @@
 #ifndef __THER_TEMP_CAL_H__
 #define __THER_TEMP_CAL_H__
 
-float temp_cal_get_res_by_adc(unsigned char channel, unsigned short adc_val);
+float temp_cal_get_res_by_ch0(unsigned short adc_val);
+float temp_cal_get_res_by_ch1(unsigned short adc_val);
 unsigned short temp_cal_get_temp_by_res(float res);
 
 #endif
